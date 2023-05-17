12642b270d0e1826d95c184e332e34ef83a27b82
Jorge Enrique Condorios Yllapuma <jcondorios@unsa.edu.pe>
2023-05-17 16:59:37 -0500

    Enviando ej1

--- Lab_02/invertirMatriz.java
+++ Lab_02/invertirMatriz.java
@@ -1,1 +1,27 @@
-s
+import java.util.Arrays;
+
+public class invertirMatriz{
+    public static void main (String args[]){
+        int[] array =  {1, 2, 3};
+        imprimirArray(invertirArray(array));
+    }
+
+    public static int[] invertirArray(int[] x){
+        int i = 0;
+        int j = x.length - 1;
+        while(i < j){
+            int k = x[i];
+            x[i] = x[j];
+            x[j] = k;
+            i++;
+            j--;
+        }
+
+        return x; 
+    }
+
+    public static void imprimirArray(int[]x){
+        for(int i: x)
+            System.out.println(i);
+    }
+}
