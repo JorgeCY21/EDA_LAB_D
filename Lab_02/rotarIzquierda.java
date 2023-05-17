d0e7baff958921f2ddb7b804e805e6b7dd275c17
Jose Luis Cusilayme Garcia <jcusilaymeg@unsa.edu.pe>
2023-05-17 17:09:35 -0500

    Enviando ejercicio 02

---
+++ Lab_02/rotarIzquierda.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class rotaIzquierda {
+    public static void main (String args[]){
+
+    
+
+    public static int[] rotarIzquierdaArray(int[] A, int d){
+    /** */
+    //Procedimiento para rotar la matriz
+    /** */
+    
+
+    return Aiz;
+    }
+
+}
