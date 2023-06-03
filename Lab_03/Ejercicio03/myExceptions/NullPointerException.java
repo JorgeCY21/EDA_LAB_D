88349f3f954cd0b95892220065e933d5fad54ea8
Carlo Joaquin Valdivia Luna <cvaldivialu@unsa.edu.pe>
2023-06-03 08:16:34 -0500

    creando la excepción NullPointerException

---
+++ Lab_03/Ejercicio03/myExceptions/NullPointerException.java
@@ -0,0 +1,11 @@
+package myExceptions;
+
+public class NullPointerException extends Exception {
+    public NullPointerException() {
+        super();
+    }
+
+    public NullPointerException(String message) {
+        super(message);
+    }
+}
