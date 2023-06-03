08560067f20a4562c4bd3f71c611779315868817
Carlo Joaquin Valdivia Luna <cvaldivialu@unsa.edu.pe>
2023-06-03 08:25:49 -0500

    autogenerando los métodos necesarios al implementar la interfaz QueueList

--- Lab_03/Ejercicio03/Queue.java
+++ Lab_03/Ejercicio03/Queue.java
@@ -1,4 +1,34 @@
 
-public class Queue {
+public class Queue<E> implements QueueList {
+
+    public boolean add(Object e) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public Object element() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public boolean offer(Object e) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public Object peek() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Object poll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Object remove() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
