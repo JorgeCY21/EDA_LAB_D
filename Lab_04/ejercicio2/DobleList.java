009585f0f9dcca1ca42d62fc0be1fc16254081dc
Alexis Baltazar Mamani Mamani <almamanima@gunsa.edu.pe>
2023-06-17 15:35:30 -0500

    Implementando algoritmo de los peores casos y el metodo para el ordenamiento

--- Lab_04/ejercicio2/DobleList.java
+++ Lab_04/ejercicio2/DobleList.java
@@ -1,14 +1,16 @@
 package lab4;
 
-public class DobleList<E> {
+public class DobleList<E extends Comparable<E>> {
     private Node<E> head;
+    private Node<E> tail;
+    private int size;
     
     public DobleList(){
         this.head  = null;
     }
 
     public boolean isEmpty(){
-        return head == null;
+        return head == null && tail ==null;
     }
     public void insertAlInicio(E data){
         Node<E> newnode = new Node<E>(data);
@@ -24,21 +26,76 @@ public class DobleList<E> {
         Node<E> newnode = new Node<E>(data);
         if(isEmpty()){
             head = newnode;
+            tail = newnode;
         }
         else{
-            newnode.setPreviousNode(newnode);
-            
+            tail.setNextNode(newnode);
+            tail = newnode;
         }
     }
-
-    @Override
-    public String toString(){
-        Node<E> nodos = head;
-        String cadena=null;
-        while (nodos != null){
-            cadena+=nodos.getData().toString()+" ";
-            nodos = nodos.getNextNode();
+    public int size(){
+        return size;
+    }
+    
+    public void generarPeoresCasos(){
+        if (isEmpty() || size==1){
+            System.out.println("La lista no tiene nada que ordenar");; 
         }
-        return cadena;
+        else{
+            Node<E> nodoActual = head;
+            while (nodoActual != null){
+                Node<E> minNode = nodoActual;
+                Node<E> comparerNode = nodoActual.getNextNode();
+                while(comparerNode != null){
+                    if(comparerNode.getData().compareTo(minNode.getData())>0){
+                        minNode=comparerNode;
+                    }
+                    comparerNode = comparerNode.getNextNode();
+                }
+                swapNodes(nodoActual,minNode);
+                nodoActual = nodoActual.getNextNode();
+            }
+        }
+    }
+    
+    public void selecctionSort(){
+        if (isEmpty() || size==1){
+            System.out.println("La lista no tiene nada que ordenar");; 
+        }
+        else{
+            Node<E> nodoActual = head;
+            while (nodoActual != null){
+                Node<E> minNode = nodoActual;
+                Node<E> comparerNode = nodoActual.getNextNode();
+                while(comparerNode != null){
+                    if(comparerNode.getData().compareTo(minNode.getData())<0){
+                        minNode=comparerNode;
+                    }
+                    comparerNode = comparerNode.getNextNode();
+                }
+                swapNodes(nodoActual,minNode);
+                nodoActual = nodoActual.getNextNode();
+            }
+        }
+    }
+    private void swapNodes(Node<E> nodeA, Node<E> nodeB) {
+        if (nodeA == nodeB) {
+            return; // Los nodos son iguales, no es necesario intercambiar
+        }
+
+        // Intercambio de elementos
+        E temp = nodeA.getData();
+        nodeA.setData(nodeB.getData());
+        nodeB.setData(temp);
+    }
+    @Override
+    public String toString() {
+        Node<E> currentNode = head;
+        StringBuilder cadena = new StringBuilder();
+        while (currentNode != null) {
+            cadena.append(currentNode.getData().toString()).append(" ");
+            currentNode = currentNode.getNextNode();
+        }
+        return cadena.toString();
     }
 }
--- Lab_04/ejercicio2/Lab4.java
+++ Lab_04/ejercicio2/Lab4.java
@@ -8,35 +8,24 @@ public class Lab4 {
         Scanner teclado = new Scanner( System.in );
         System.out.print( "Introduzca el tamaño del arreglo: " );
        // tamano = teclado.nextInt();
-        //int arreglo[] = generarPeorCaso(tamano);
+        //int arreglo[] = generarPeorCaso(ta|mano);
         
         DobleList <Integer> listDoble = new DobleList<>();
-        listDoble.insertAlInicio(1);
-        listDoble.insertAlInicio(2);
+        for(int i=0;i<=100;i++){
+            listDoble.insertFinal(i);
+        }
+        System.out.println(listDoble.toString());
+        listDoble.generarPeoresCasos();
         System.out.println(listDoble.toString());
         
-        }
+        long nano_start = System.nanoTime();
+        listDoble.selecctionSort();
+        long nano_end = System.nanoTime();
+        System.out.println(nano_end-nano_start);
+    }
     
         
-        //Modificación del algoritmo de inserción para devolver tiempo de ejecición:
-        public static long insertionSort(int[] A) {
-        int key;
-        int i;
-        long nano_startTime = System.nanoTime();
-        for(int j=1; j<A.length; j=j+1) {
-        key = A[j];
-        //Insertar A[j] en la secuencia ordenada A[1..j-1]
-        i = j-1;
-        while(i>-1 && A[i]>key) {
-        A[i+1] = A[i];
-        i = i-1;
-        }
-        A[i+1] = key;
-        //imprimirArreglo(A);
-        }
-        long nano_endTime = System.nanoTime();
-        return nano_endTime - nano_startTime;
-        }
-
+        
+        
 }
 
