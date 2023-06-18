f2f86638a9807a55502415649fe8d6980ec7ec08
Alexis Baltazar Mamani Mamani <almamanima@gunsa.edu.pe>
2023-06-17 19:08:46 -0500

    Terminando ejercicio 2

--- Lab_04/Ejercicio02/DobleList.java
+++ Lab_04/Ejercicio02/DobleList.java
@@ -58,26 +58,62 @@ public class DobleList<E extends Comparable<E>> {
         }
     }
     
-    public void selecctionSort(){
-        if (isEmpty() || size==1){
-            System.out.println("La lista no tiene nada que ordenar");; 
-        }
-        else{
-            Node<E> nodoActual = head;
-            while (nodoActual != null){
-                Node<E> minNode = nodoActual;
-                Node<E> comparerNode = nodoActual.getNextNode();
-                while(comparerNode != null){
-                    if(comparerNode.getData().compareTo(minNode.getData())<0){
-                        minNode=comparerNode;
-                    }
-                    comparerNode = comparerNode.getNextNode();
-                }
-                swapNodes(nodoActual,minNode);
-                nodoActual = nodoActual.getNextNode();
-            }
-        }
+public void insertionSort() {
+    if (head == null || head.getNextNode() == null) {
+        return; // La lista está vacía o tiene solo un elemento, no es necesario ordenar
     }
+
+    Node<E> sortedTail = head; // Puntero al final de la sección ordenada de la lista
+
+    // Iterar desde el segundo nodo hasta el final de la lista
+    Node<E> currentNode = head.getNextNode();
+    while (currentNode != null) {
+        E currentData = currentNode.getData();
+
+        // Buscar la posición correcta para insertar el nodo actual en la sección ordenada
+        Node<E> insertionPoint = sortedTail;
+        while (insertionPoint != null && insertionPoint.getData().compareTo(currentData) > 0) {
+            insertionPoint = insertionPoint.getPreviousNode();
+        }
+
+        // Mover los nodos para insertar el nodo actual en la posición correcta
+        if (insertionPoint == null) {
+            // El nodo actual debe ser el nuevo inicio de la lista
+            sortedTail.setNextNode(currentNode.getNextNode());
+            if (currentNode.getNextNode() != null) {
+                currentNode.getNextNode().setPreviousNode(null);
+            }
+            currentNode.setNextNode(head);
+            head.setPreviousNode(currentNode);
+            head = currentNode;
+        } else {
+            // Insertar el nodo actual después del punto de inserción
+            sortedTail.setNextNode(currentNode.getNextNode());
+            if (currentNode.getNextNode() != null) {
+                currentNode.getNextNode().setPreviousNode(sortedTail);
+            }
+            currentNode.setNextNode(insertionPoint.getNextNode());
+            if (insertionPoint.getNextNode() != null) {
+                insertionPoint.getNextNode().setPreviousNode(currentNode);
+            }
+            insertionPoint.setNextNode(currentNode);
+            currentNode.setPreviousNode(insertionPoint);
+        }
+
+        // Actualizar el puntero al final de la sección ordenada
+        if (sortedTail.getNextNode() != null) {
+            sortedTail = sortedTail.getNextNode();
+        }
+
+        // Mover al siguiente nodo para su comparación e inserción
+        currentNode = sortedTail.getNextNode();
+    }
+}
+
+
+
+
+
     private void swapNodes(Node<E> nodeA, Node<E> nodeB) {
         if (nodeA == nodeB) {
             return; // Los nodos son iguales, no es necesario intercambiar
