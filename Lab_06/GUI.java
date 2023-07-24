781048d53fe36683ce31b1ecd6cde8a1cf468aba
Carlo Joaquin Valdivia Luna <cvaldivialu@unsa.edu.pe>
2023-07-24 09:46:49 -0500

    terminando el codigo

--- Lab_06/GUI.java
+++ Lab_06/GUI.java
@@ -11,25 +11,37 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.Timer;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 public class GUI extends JFrame {
 
 	private final int WIDTH = 450;
-	private final int HEIGHT = 500;
-	
+	private final int HEIGHT = 250;
+
 	private final int WIDTH_CONTENT_PANEL = WIDTH - 40;
-	private final int HEIGHT_CONTENT_PANEL = 200;
-	
+	private final int HEIGHT_CONTENT_PANEL = 133;
+	private static final int DELAY = 1000;
+
 	private JPanel bodyPanel;
-	private PageBuscar pageBuscar = new PageBuscar(WIDTH_CONTENT_PANEL, HEIGHT_CONTENT_PANEL);
-	private PageReemplazar pageReemplazar = new PageReemplazar();
-	private PageIrA pageIrA = new PageIrA();
+	private Trie trie;
+	
+	private PageBuscar pageBuscar = new PageBuscar(this, trie);
+	private PageReemplazar pageReemplazar = new PageReemplazar(this, trie);
+	private PageIrA pageIrA = new PageIrA(this, trie);
+	
 	private JButton btnBuscar;
 	private JButton btnReemplazar;
 	private JButton btnIrA;
+	
+	private JTextArea textArea;
+	private Timer timer;
+	
 	private JPanel contentPanel;
+	private boolean estaExpandido = false;
 
 	public static void main(String[] args) {
 		new GUI();
@@ -41,24 +53,24 @@ public class GUI extends JFrame {
 		setTitle("Editor de Texto");
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		bodyPanel = new JPanel();
 		bodyPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(bodyPanel);
 		bodyPanel.setLayout(null);
-		
+
 		content();
-		
-		setVisible(true);		
+
+		setVisible(true);
 	}
-	
+
 	private void content() {
 		JLabel label = new JLabel("Texto:");
 		label.setFont(new Font("Roboto Black", Font.PLAIN, 12));
 		label.setBounds(10, 11, 83, 23);
 		bodyPanel.add(label);
-		
+
 		btnBuscar = newJButton("Buscar", 10, 170, 70, 23, null, null);
 		btnBuscar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -66,7 +78,7 @@ public class GUI extends JFrame {
 			}
 		});
 		bodyPanel.add(btnBuscar);
-		
+
 		btnReemplazar = newJButton("Reemplazar", 85, 170, 95, 23, null, null);
 		btnReemplazar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -74,7 +86,7 @@ public class GUI extends JFrame {
 			}
 		});
 		bodyPanel.add(btnReemplazar);
-		
+
 		btnIrA = newJButton("Ir a", 185, 170, 45, 23, null, null);
 		btnIrA.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -82,43 +94,91 @@ public class GUI extends JFrame {
 			}
 		});
 		bodyPanel.add(btnIrA);
-		
+
 		contentPanel = new JPanel();
 		contentPanel.setBounds(10, 200, WIDTH_CONTENT_PANEL, HEIGHT_CONTENT_PANEL);
 		bodyPanel.add(contentPanel);
-		
-		showPanel(pageBuscar);
-		
-		JTextArea textArea = new JTextArea(5, 20); // 5 filas y 20 columnas
+
+		textArea = new JTextArea(5, 20);
 		textArea.setBackground(new Color(255, 255, 255));
 		textArea.setLineWrap(true);
 		textArea.setWrapStyleWord(true);
-			
+
+		textArea.getDocument().addDocumentListener(new DocumentListener() {
+			@Override
+			public void insertUpdate(DocumentEvent e) {
+				restartTimer();
+			}
+
+			@Override
+			public void removeUpdate(DocumentEvent e) {
+				restartTimer();
+			}
+
+			@Override
+			public void changedUpdate(DocumentEvent e) {
+			}
+		});
+
+		timer = new Timer(DELAY, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				timer.stop();
+				updateTrie();
+			}
+		});
+
 		JScrollPane scrollPane = new JScrollPane(textArea);
-        scrollPane.setBounds(20, 45, WIDTH_CONTENT_PANEL - 10, 100);
-        bodyPanel.add(scrollPane);
+		scrollPane.setBounds(20, 45, WIDTH_CONTENT_PANEL - 10, 100);
+		bodyPanel.add(scrollPane);
 	}
-	
+
+	private void restartTimer() {
+		timer.restart();
+	}
+
+	private void updateTrie() {
+		String text = textArea.getText();
+		trie = new Trie(text);
+		System.out.println(trie);
+	}
+
 	private void showPanel(JPanel panel) {
+		if (!estaExpandido) {
+			setSize(WIDTH, HEIGHT + 130);
+			setLocationRelativeTo(null);
+		}
+
 		panel.setSize(WIDTH_CONTENT_PANEL, HEIGHT_CONTENT_PANEL);
 		panel.setLocation(0, 0);
-		
+
 		contentPanel.removeAll();
+		contentPanel.setLayout(new BorderLayout());
 		contentPanel.add(panel);
+		contentPanel.revalidate();
 		contentPanel.repaint();
-		
-		System.out.println(panel);
 	}
-	
+
 	private JButton newJButton(String text, int x, int y, int width, int height, Color border, Color background) {
 		JButton btn = new JButton(text);
-		
+
 		btn.setFocusPainted(false);
 		btn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btn.setBorder(new LineBorder((border != null) ? border : new Color (0, 0, 0)));
+		btn.setBorder(new LineBorder((border != null) ? border : new Color(0, 0, 0)));
 		btn.setBackground((background != null) ? background : new Color(215, 215, 215));
 		btn.setBounds(x, y, width, height);
-		
-		return btn;		
+
+		return btn;
+	}
+
+	public void ocultar() {
+		setSize(WIDTH, HEIGHT);
+		setLocationRelativeTo(null);
+		estaExpandido = false;
+		contentPanel.removeAll();
+	}
+
+	public JTextArea getTextArea() {
+		return textArea;
 	}
 }
--- Lab_06/TrieNode.java
+++ Lab_06/TrieNode.java
@@ -1,27 +1,29 @@
-public class TrieNode {
+class TrieNode {
+	private TrieNode[] children;
+	private boolean isEndWord;
 
-    private TrieNode[] children;
-    private boolean isEndWord;
+	public TrieNode() {
+		this.children = new TrieNode[255];
+		this.isEndWord = false;
+	}
 
-    public TrieNode() {
-        this.children = new TrieNode[128];
-        this.isEndWord = false;
-    }
+	public boolean isEndWord() {
+		return this.isEndWord;
+	}
 
-    public boolean isEndWord() {
-        return this.isEndWord;
-    }
+	public void setIsEndWord(boolean isEndWord) {
+		this.isEndWord = isEndWord;
+	}
 
-    public void setIsEndWord(boolean isEndWord) {
-        this.isEndWord = isEndWord;
-    }
+	public TrieNode getChild(char c) {
+		return this.children[c];
+	}
 
-    public TrieNode getChild(char c) {
-        return this.children[c];
-    }
+	public void setChild(char c, TrieNode node) {
+		this.children[c] = node;
+	}
 
-    public void setChild(char c, TrieNode node) {
-        this.children[c]= node;
-    }
-
+	public int getChildrenLength() {
+		return this.children.length;
+	}
 }
